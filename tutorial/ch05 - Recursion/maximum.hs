da33e1051fd940993dbf28357e3d7f5a7349a358
adrian <adrian.lin.1.0.0@gmail.com>
2023-03-21 18:05:21 +0800

    feat : maximum, repeat, replicate, reverse ,take

---
+++ tutorial/ch05 - Recursion/maximum.hs
@@ -0,0 +1,13 @@
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x : xs)
+  | x > maxTail = x
+  | otherwise = maxTail
+  where
+    maxTail = maximum' xs
+
+maximum'' :: (Ord a) => [a] -> a
+maximum'' [] = error "maximum of empty list"
+maximum'' [x] = x
+maximum'' (x:xs) = max x (maximum'' xs)
