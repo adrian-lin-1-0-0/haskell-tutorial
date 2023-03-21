a1fd2da59ccc2e3242a23bfbf6bdd0874e95cd1f
adrian <adrian.lin.1.0.0@gmail.com>
2023-03-21 15:56:26 +0800

    feat : 'let' in ch04

--- tutorial/ch04 - Synatx in Functions/bmi.hs
+++ tutorial/ch04 - Synatx in Functions/bmi.hs
@@ -12,3 +12,6 @@ calcBmis :: (RealFloat a) => [(a, a)] -> [a]
 calcBmis xs = [bmi w h | (w, h) <- xs]
   where
     bmi weight height = weight / height ^ 2
+
+calcBmis' :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis' xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]
