a88c48b915ca38b757696ac39c87c846ac9f83ef
adrian <adrian.lin.1.0.0@gmail.com>
2023-03-24 09:48:57 +0800

    feat : export Shapes

--- tutorial/ch08 - Making Our Own Types and Tpyeclasses/shape.hs
+++ tutorial/ch08 - Making Our Own Types and Tpyeclasses/shape.hs
@@ -1,3 +1,12 @@
+module Shapes
+  ( Point (..),
+    Shape (..),
+    surface,
+    nudge,
+    baseCircle,
+  )
+where
+
 data Point = Point Float Float deriving (Show)
 
 data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
